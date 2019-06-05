d873cf4585de7a0dfbb23b3558b2a1328da8e0aa
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2019-06-05 10:15:02 -0300

    Adding template

---
+++ base.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " = " << x << endl
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL)
+
+
+int main(int argc, char const *argv[])
+{
+    IOS;
+    return 0;
+}
