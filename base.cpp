0ec77e23628216abf07bddf3debc2d5ff017face
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2019-06-13 11:31:17 -0300

    Add GCD and removing trash files

--- base.cpp
+++ base.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 #define debug(x) cerr << #x << " = " << (x) << endl
 #define IOS ios::sync_with_stdio(false); cin.tie(NULL)
-
+// Greate commom divisor
+#define gcd(a, b) __gcd(a, b) 
 
 int main(int argc, char const *argv[])
 {
